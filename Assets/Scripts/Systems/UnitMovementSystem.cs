ec09572fd83011430fdf2d4a31ad4d040919811e
pajetic <mssjimmy@gmail.com>
2025-09-09 23:51:29 -0400

    Testing out jobs with unit movement

--- Assets/Scripts/Systems/UnitMovementSystem.cs
+++ Assets/Scripts/Systems/UnitMovementSystem.cs
@@ -8,22 +8,37 @@ partial struct UnitMovementSystem : ISystem {
     
     [BurstCompile]
     public void OnUpdate(ref SystemState state) {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<UnitMovement> unitMovement, RefRW<PhysicsVelocity> physicsVelocity) 
-                 in SystemAPI.Query<RefRW<LocalTransform>, RefRO<UnitMovement>, RefRW<PhysicsVelocity>>()) {
+        new UnitMovementJob {
+            deltaTime = SystemAPI.Time.DeltaTime,
+        }.ScheduleParallel();
+    }
 
-            float3 moveDirection = math.normalize(unitMovement.ValueRO.TargetPosition - localTransform.ValueRO.Position);
+    [BurstCompile]
+    public partial struct UnitMovementJob : IJobEntity {
+        public float deltaTime;
+        public void Execute(RefRW<LocalTransform> localTransform, RefRO<UnitMovement> unitMovement, RefRW<PhysicsVelocity> physicsVelocity) {
+            float destinationDistanceTolerance = 0.3f; // TODO refactor 
             
+            float3 moveDirection = unitMovement.ValueRO.TargetPosition - localTransform.ValueRO.Position;
+
+            // Stop movement when we are close enough
+            if (math.lengthsq(moveDirection) < destinationDistanceTolerance) {
+                physicsVelocity.ValueRW.Linear = float3.zero;
+                physicsVelocity.ValueRW.Angular = float3.zero;
+                return;
+            }
+            
+            moveDirection = math.normalize(moveDirection);
             localTransform.ValueRW.Rotation = 
                 math.slerp(localTransform.ValueRO.Rotation,
                     quaternion.LookRotation(moveDirection, math.up()),
-                    unitMovement.ValueRO.RotationSpeed * SystemAPI.Time.DeltaTime);
+                    unitMovement.ValueRO.RotationSpeed * deltaTime);
             
             // Physics movement
             physicsVelocity.ValueRW.Linear = moveDirection * unitMovement.ValueRO.MoveSpeed;
             
             // Stop rotation on collisions
             physicsVelocity.ValueRW.Angular = float3.zero;
-        } 
+        }
     }
-
 }
