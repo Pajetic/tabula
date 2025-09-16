24d0955002add055c21d75ed8e3ef4f5b1d32941
pajetic <mssjimmy@gmail.com>
2025-09-16 17:30:04 -0400

    Enemy unit + factions logic

--- Assets/Scripts/Authoring/UnitAuthoring.cs
+++ Assets/Scripts/Authoring/UnitAuthoring.cs
@@ -2,14 +2,17 @@ using Unity.Entities;
 using UnityEngine;
 
 public class UnitAuthoring : MonoBehaviour {
+    public Faction Faction;
     public class Baker : Baker<UnitAuthoring> {
         public override void Bake(UnitAuthoring authoring) {
             Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-            AddComponent(entity, new Unit());
+            AddComponent(entity, new Unit {
+                Faction = authoring.Faction,
+            });
         }
     }
 }
 
 public struct Unit : IComponentData {
-    
+    public Faction Faction;
 }
